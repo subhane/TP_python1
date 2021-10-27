94cdb9ba6f9a964571a27459b0d9fe5891352c17
Subhane JOOHN MOUHAMAD <subhanej@gmail.com>
2021-10-27 17:19:55 +0200

    question5

--- main.py
+++ main.py
@@ -21,17 +21,25 @@ def compte_triangle(n):
     return s
 
 def nb_differences(s1,s2):
-    s=0
-    if len(s1)<len(s2):
+    s = 0
+    if len(s1) < len(s2):
         n = len(s1)
     else:
-        n  = len(s2)
+        n = len(s2)
     for i in range(n):
-       if s1[i]!=s2[i]:
-           s= s+1
+        if s1[i] != s2[i]:
+           s = s+1
     return s
 
 print(nb_differences("oiseau","bateau"))
+
+def triangle_motif(n):
+    a = 2*n-1
+    b = int((a-1)/2)
+    for i in range(n):
+        c = a-2*(b-i)
+        print((b-i)*" "+c*"*"+(b-i)*" ")
+triangle_motif(13)
 
 
 
