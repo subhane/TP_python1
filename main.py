0f50595c11b463269f89fe5630291bfa7164bbbf
Subhane JOOHN MOUHAMAD <subhanej@gmail.com>
2021-10-27 15:35:07 +0200

    question2

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import math
 
-valeur1 = input("Saisir valeur 1 : ")
-valeur2 = input("Saisir valeur 2 : ")
+def division() :
+    valeur1 = input("Saisir valeur 1 : ")
+    valeur2 = input("Saisir valeur 2 : ")
 
-print(valeur1/valeur2)
-print(valeur1%valeur2)
+    print(valeur1/valeur2)
+
+def modulo():
+    valeur1 = input("Saisir valeur 1 : ")
+    valeur2 = input("Saisir valeur 2 : ")
+    print(valeur1%valeur2)
 
 def calculAir(r):
     return 2*math.pi*r**2
+
+def compte_triangle(n):
+    s = 0
+    for i in range(1,n+1):
+        s = s+i
+    return s
+
 
 
 
