22b7351b88ab03e1ff53b228b945682124048445
Subhane JOOHN MOUHAMAD <subhanej@gmail.com>
2021-10-27 15:28:59 +0200

    question1

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+
+valeur1 = input("Saisir valeur 1 : ")
+valeur2 = input("Saisir valeur 2 : ")
+
+print(valeur1/valeur2)
+print(valeur1%valeur2)
+
+def calculAir(r):
+    return 2*math.pi*r**2
+
+
+
