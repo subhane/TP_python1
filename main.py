8f377860dbf3f9e320d2c24e17fa8483a803fc4a
Subhane JOOHN MOUHAMAD <subhanej@gmail.com>
2021-10-27 16:18:39 +0200

    question4

--- main.py
+++ main.py
@@ -9,16 +9,30 @@ def division() :
 def modulo():
     valeur1 = input("Saisir valeur 1 : ")
     valeur2 = input("Saisir valeur 2 : ")
-    print(valeur1%valeur2)
+    print(valeur1 % valeur2)
 
 def calculAir(r):
     return 2*math.pi*r**2
 
 def compte_triangle(n):
     s = 0
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         s = s+i
     return s
+
+def nb_differences(s1,s2):
+    s=0
+    if len(s1)<len(s2):
+        n = len(s1)
+    else:
+        n  = len(s2)
+    for i in range(n):
+       if s1[i]!=s2[i]:
+           s= s+1
+    return s
+
+print(nb_differences("oiseau","bateau"))
+
 
 
 
